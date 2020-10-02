51a6d46047ce7a1b4486fc12728481c25226e343
Ayush Porwal <f20171090@hyderabad.bits-pilani.ac.in>
2020-10-02 21:39:40 +0530

    hid the key

--- src/api/zomato.js
+++ src/api/zomato.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import * as data from '../../key.json';
+
+const { key } = data;
 
 export default axios.create({
     baseURL: 'https://developers.zomato.com/api/v2.1',
     headers: {
         "Accept": "application/json",
-        "user-key": "e8bcc0d533aa257fe1cd32732e90f8a6"
+        "user-key": key,
     }
 });
